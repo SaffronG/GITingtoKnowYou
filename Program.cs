997d3568c8a8544fc8dae2c336d0e8c09b78a53a
SaffronGamer <saffrongamer167@gmail.com>
2025-01-29 10:49:19 -0700

    Added question

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿Console.WriteLine("I am GITing tired of learning Git...");
 
+Console.Write("What is your favorite programming language?");
+string choice = Console.ReadLine()!;
+File.WriteAllText($"{choice}.txt", choice);
+// Mine is Rust! ~ Chase
--- obj/Debug/net8.0/GITingtoKnowYou.AssemblyInfo.cs
+++ obj/Debug/net8.0/GITingtoKnowYou.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GITingtoKnowYou")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a75aea328912051bd5a1155aac5eb8ecbad1e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33804606259d875e3d4c14ba81c48c7e50844105")]
 [assembly: System.Reflection.AssemblyProductAttribute("GITingtoKnowYou")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GITingtoKnowYou")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
