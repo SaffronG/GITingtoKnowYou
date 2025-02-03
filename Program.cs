75a7511926c9ad0ba21ed0a751184fe9d50c51d6
Tyson Meade <tyson.meade@students.snow.edu>
2025-02-03 11:00:53 -0700

    Answer to Question 2

--- Program.cs
+++ Program.cs
@@ -3,3 +3,5 @@
 // Question 1: What is your favorite flavor of soda (or drink if you don't drink soda)?
 // Mtn Dew Baja Blast or Dr. Pepper.
 // Mtn Dew Major Melon
+
+// My favorite video game is Undertale.
